793a93007436341b44fa3d75ef9086c5e7a24a4b
jyotiraditya <jyotiraditya24@gmail.com>
2024-04-22 00:48:30 +0530

    Linked List to Queue Adapter

--- Data Structures/DSA/src/LinkedList/LinkedListToQueueAdapter.java
+++ Data Structures/DSA/src/LinkedList/LinkedListToQueueAdapter.java
@@ -1,4 +1,31 @@
 package LinkedList;
 
+import java.util.LinkedList;
+
 public class LinkedListToQueueAdapter {
+    LinkedList<Integer> list = new LinkedList<>();
+
+    public int size(){
+        return list.size();
+    }
+//    Add Last
+    public void add(int val){
+        list.addFirst(val);
+    }
+//    remove First
+    public int remove(){
+        if(list.isEmpty()){
+            return  -1;
+        }else {
+           return list.removeFirst();
+        }
+    }
+
+    public int peek(){
+        if(list.isEmpty()){
+            return  -1;
+        }else {
+            return list.peek();
+        }
+    }
 }
