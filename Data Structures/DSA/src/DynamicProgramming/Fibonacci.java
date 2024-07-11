be1f358bd0f2a4cae2766854a45f54ed88ae0e41
jyotiraditya <jyotiraditya24@gmail.com>
2024-07-12 00:56:45 +0530

    fib using dp

--- Data Structures/DSA/src/DynamicProgramming/Fibonacci.java
+++ Data Structures/DSA/src/DynamicProgramming/Fibonacci.java
@@ -1,25 +1,38 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
+
 public class Fibonacci {
     public static void main(String[] args) {
-        int val = 20;
-        int[]arr = new int[val+1];
-        for (int i = 0; i <= val; i++) {
-            arr[i] = -1;
+//        int val = 20;
+//        int[]arr = new int[val+1];
+//        for (int i = 0; i <= val; i++) {
+//            arr[i] = -1;
+//        }
+//        int result = DpFibonacci(val,arr);
+//        System.out.println(result);
+
+//        for tabulation method
+        int val = 10;
+        int []arr = new int[val+1];
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for(int i = 2;i<=val;i++){
+            arr[i] = arr[i-1] + arr[i-2];
         }
-        int result = DpFibonacci(val,arr);
-        System.out.println(result);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static int DpFibonacci(int val,int[]arr){
-        if(val <= 1){
+    public static int DpFibonacci(int val,int[]arr) {
+        if (val <= 1) {
             return val;
         }
 
-        if(arr[val]!=-1){
-           return arr[val];
+        if (arr[val] != -1) {
+            return arr[val];
         }
-        arr[val] = DpFibonacci(val - 1,arr ) + DpFibonacci(val-2,arr);
+        arr[val] = DpFibonacci(val - 1, arr) + DpFibonacci(val - 2, arr);
         return arr[val];
     }
 }
