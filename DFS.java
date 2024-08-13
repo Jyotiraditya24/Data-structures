abcb7f7522b788af855911db780f37c3ee07d5e0
jyotiraditya <93746326+Jyotiraditya24@users.noreply.github.com>
2024-08-14 02:24:24 +0530

    DFS recursive
    recursive option

---
+++ DFS.java
@@ -0,0 +1,79 @@
+package StriverGraph;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class DFS {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int V = 8;
+        for(int i = 0 ;i<=V; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        adj.get(1).add(2);
+        adj.get(1).add(3);
+
+        adj.get(2).add(1);
+        adj.get(2).add(5);
+        adj.get(2).add(6);
+
+        adj.get(3).add(1);
+        adj.get(3).add(4);
+        adj.get(3).add(7);
+
+        adj.get(4).add(8);
+        adj.get(4).add(3);
+
+        adj.get(5).add(2);
+
+        adj.get(6).add(2);
+
+        adj.get(7).add(3);
+        adj.get(7).add(8);
+
+        adj.get(8).add(4);
+        adj.get(8).add(7);
+
+
+//        ArrayList<Integer> dfs =  DfsOfGraph(adj,V);
+//        System.out.println(dfs);
+        boolean[]visited = new boolean[V+1];
+        ArrayList<Integer> ans = new ArrayList<>();
+        dfs(1,visited,adj,ans);
+        System.out.println(ans);
+    }
+
+    public static ArrayList<Integer> DfsOfGraph(ArrayList<ArrayList<Integer>> adj,int V){
+        boolean[]visited = new boolean[V+1];
+        Stack<Integer> st = new Stack<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        st.push(1);
+        visited[1] = true;
+
+        while (!st.isEmpty()){
+            int node = st.pop();
+            ans.add(node);
+            ArrayList<Integer> list =  adj.get(node);
+            for(Integer i :list){
+                if(!visited[i]){
+                    st.push(i);
+                    visited[i] = true;
+                }
+            }
+        }
+        return  ans;
+    }
+
+    public static void dfs(int node, boolean[] visited, ArrayList<ArrayList<Integer>> adj,ArrayList<Integer> ans){
+        visited[node] = true;
+        ans.add(node);
+        for(Integer i: adj.get(node)){
+            if(!visited[i]){
+                dfs(i,visited,adj,ans);
+            }
+        }
+    }
+
+}
